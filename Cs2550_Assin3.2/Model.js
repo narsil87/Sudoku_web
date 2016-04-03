409527923768ebc50e2c2e0c1c370866bf1743e0
narsil87 <twissler87@hotmail.com>
2016-04-02 23:17:11 -0600

    project

---
+++ Cs2550_Assin3.2/Model.js
@@ -0,0 +1,200 @@
+﻿function Login() {
+    loadSyncPost();
+}
+
+function ClearLocalStorage() {
+    alert("Local storage cleared");
+    localStorage.clear();
+}
+console.log(loadSyncPost);
+
+//function loadSyncXML() {
+//    var name = document.getElementById("userinfo.xml").value;
+//    var data = "name=" + name;
+//    var localRequest = new XMLHttpRequest();
+
+//    // PASSING false AS THE THIRD PARAMETER TO open SPECIFIES SYNCHRONOUS
+//    localRequest.open("POST", "data.php", false);
+//    localRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//    localRequest.send(data);
+
+//    // NOTE THAT THE status WILL NOT BE 200 IF THE REQUEST IS FOR A
+//    // LOCAL FILE.
+
+//    var dataDiv = document.getElementById("userDataDiv");
+
+//    // FOR MORE INFORMATION ABOUT JSON SEE http://json.org
+//    var responseJson = JSON.parse(localRequest.responseText);
+//    dataDiv.innerHTML = "Your password is: " + responseJson["password"];
+    
+//}
+console.log(loadDoc);
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4) {
+            myFunction(xhttp);
+        }
+    };
+    xhttp.open("GET", "userinfo.xml", true);
+    xhttp.send();
+}
+function myFunction(xml) {
+    alert("xml pressed");
+    var i;
+    var xmlDoc = xml.responseXML;
+    var table = "<tr><th>name</th><th>email</th></tr><tr><th>userid</th><th>score</th></tr>";
+    var x = xmlDoc.getElementsByTagName("user");
+    for (i = 0; i < x.length; i++) {
+        table += "<tr><td>" +
+        x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("email")[0].childNodes[0].nodeValue +
+        "</td></tr>";
+        x[i].getElementsByTagName("userid")[0].childNodes[0].nodeValue +
+       "</td><td>" +
+       x[i].getElementsByTagName("score")[0].childNodes[0].nodeValue +
+       "</td></tr>";
+    }
+    document.getElementById("userDataDiv").innerHTML = table;
+    window.open("sudoku.html");
+}
+
+function loadSyncPost() {
+   alert("login pressed");
+    var name = document.getElementById("NameInput").value;
+    var password = document.getElementById("PassInput").value;
+    var data = "userName="+ name+ "&password="+ password;
+    var ajax = new XMLHttpRequest();
+
+    ajax.open("POST", "http://universe.tc.uvu.edu/cs2550/assignments/PasswordCheck/check.php", false);
+    ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    ajax.send(data);
+    jsonText = JSON.parse(ajax.responseText);
+    //get div to dipslay output
+    var dataDiv = document.getElementById("LoginR");
+    var userNDiv = document.getElementById("userNameDiv");
+
+    if (ajax.status == 200 && jsonText.result=="valid") {
+        //get name, password and time stamp
+        var loginText = jsonText.userName + " "+ jsonText.timestamp;
+        //Stores user info in Local Storage
+        window.localStorage.setItem("cs2550timestamp", loginText);
+        //displays success of login is accepted
+        dataDiv.innerHTML = "Login was successful!";
+        userNDiv.innerHTML = loginText;
+        
+        //when login succeed the sudoku grid window opens
+        window.open("sudoku.html");
+    } else {
+        //displays message if input was invalid
+        dataDiv.innerHTML = "Invalid user name and password.";
+    }
+}
+
+//play game event
+function playGameEvent() {
+    alert("play game event has been pressed been ");
+}
+
+//
+function cellClickedEvent(cell) {
+    alert("cell click");
+    cell.innerHTML = '3';
+    box.HtmlElement = cell[i];
+}
+
+
+function resetGame() {
+    alert("reset called");
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].innerHTML = '&nbsp;';
+    }
+}
+// Updates the HTML element to display the current value
+SudokuBox.prototype.UpdateValue = function () {
+    if (this.HtmlElement == null)
+        return;
+
+    this.HtmlElement.innerHTML = this.Value;
+};
+
+//get id
+SudokuBox.prototype.getId = function () {
+    return this.Id;
+}
+//get uniq cell Id
+SudokuBox.prototype.getBlock = function () {
+    return this.Block;
+}
+SudokuBox.prototype.getUniqId = function () {
+    return this.UniqueId;
+}
+SudokuBox.prototype.getValue = function () {
+    return this.Value;
+}
+//not sure
+var cell = this.Id + '_' + this.Block;
+
+function SudokuBox(id, block) {
+    this.Id = id;
+    this.Block = block;
+    this.HtmlElement = null;
+    this.UniqueId = this.Id + '_' + this.Block.Id;
+
+    this.Value = 0;
+}
+var UI = block + "_" + box;
+var block;
+var box;
+var blockIdx;
+var boxIdx;
+for (blockIdx = 0; blockIdx < sudoGrid.AllBlocks.length; blockIdx++) {
+    block = sudoGrid.AllBlocks[blockIdx];
+
+    for (boxIdx = 0; boxIdx < block.Boxes.length; boxIdx++) {
+        box = sudoGrid.AllBoxes[boxIdx];
+        box.UpdateValue();
+    }
+}
+function getTableLocation() {
+    document.getElementById('tr').innerHTML = this.cells;
+}
+
+var cells = document.getElementById('tr');
+function submit() {
+    var name = document.getElementById("NameInput");
+    var password = document.getElementById("PassInput");
+    var age = document.getElementById("AgeInput");
+    var gender = document.getElementById("gender");
+    var data1 = "name:" + name.value;
+    var data2 = "password:" + password.value;
+    var data3 = "Age:" + age.value;
+    var data4 = "gender:" + gender.value;
+
+    var totalData = data1 + " " + data2 + " " + data3+" "+data4
+    var coBox = document.getElementById("comBox");
+    coBox.value = totalData;
+    alert("user data recived");
+}
+function moveCake() {
+    var time = 0;
+    alert("move down");
+    var cakeImg = document.getElementById("cakeImg");
+    cakeImg.style.top = "654px";
+    time += top;
+    if (time < 5000) {
+        setTimeout(moveCake, 100);
+    }
+}
+
+//}
+function moveup() {
+    alert("move up");
+    var cakeImg = document.getElementById("cakeImg");
+    cakeImg.style.top = "-1px";
+}
+
+
+
---
+++ Cs2550_Assin3.2/main.js
@@ -0,0 +1,131 @@
+﻿// Setup game
+window.onload = function () {
+    LoadMainGrid();
+}
+
+//M
+
+function SudokuGrid() {
+    this.RowColumn_Count = 9;
+    this.AllBlocks = [];
+    this.AllBoxes = [];
+
+    // Setup blocks and boxes
+    for (var block_id = 0; block_id < this.RowColumn_Count; ++block_id) {
+        var block = new SudokuBlock(block_id);
+
+        // Add boxes to block
+        for (var box_id = 0; box_id < this.RowColumn_Count; ++box_id) {
+            var box = new SudokuBox(box_id, block);
+            // Add box to block
+            block.Boxes.push(box);
+            // Add to all boxes
+            this.AllBoxes.push(box);
+        }
+        // Add block to Blocks
+        this.AllBlocks.push(block);
+    }
+}
+
+function SudokuBox(id, block) {
+    this.Id = id;
+    this.Block = block;
+    this.HtmlElement = null;
+    this.UniqueId = this.Id + '_' + this.Block.Id;
+
+    this.Value = 0;
+}
+// Updates the HTML element to display the current value
+SudokuBox.prototype.UpdateValue = function () {
+    if (this.HtmlElement == null)
+        return;
+
+    this.HtmlElement.innerHTML = this.Value;
+};
+
+
+
+function SudokuBlock(id) {
+    this.Id = id;
+    this.Boxes = [];
+}
+
+//get coordinate of cell and changes the cell value
+function GetCellCoord(cell) {
+    var x = cell.cellIndex;
+    var y =cell.parentNode.rowIndex;
+    document.getElementById("DisCelCoor").innerHTML = y + "_" + x;
+    cell.innerHTML = cell.cellIndex;
+    box.HtmlElement = cell[i];
+}
+// Get a random 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+var rowCount = 9;
+var cellCoount = 9;
+var rows = [];
+var cells = [];
+
+var rowIdx;
+var cellIdx;
+var newRow;
+var newCell;
+var cellCord;
+// Game Init
+function LoadMainGrid() {
+    var mainGrid = document.getElementById('mainGrid');
+    var sudoGrid = new SudokuGrid();
+
+    // Create table view
+    for (rowIdx = 0; rowIdx < sudoGrid.RowColumn_Count; rowIdx++) {
+        // New <tr>
+        newRow = document.createElement('tr');
+       
+        // Set Block style
+        if ((rowIdx + 1) % 3 == 0)
+            newRow.setAttribute('class', 'tr_border');
+
+
+        // Create <td>'s
+        for (cellIdx = 0; cellIdx < sudoGrid.RowColumn_Count; cellIdx++) {
+            newCell = document.createElement('td');
+            //m
+            newCell.id = rowIdx + '_' + cellIdx;
+            cellCord = newCell.id;
+            //m
+            newCell.innerHTML = getRandomInt(1, 10);
+            //m
+            //get coordinate of cell and changes the cell value
+            newCell.onclick = function (e) {
+                GetCellCoord(this);
+            }
+            newRow.appendChild(newCell);
+            cells.push(newCell);
+        }
+        //matbe
+        rows.push(newRow);
+        mainGrid.appendChild(newRow);
+        
+    }
+}
+
+
+ //Link Model 
+for (var blockIdx = 0; blockIdx < sudoGrid.AllBlocks.length; blockIdx++) {
+    var block = sudoGrid.AllBlocks[blockIdx];
+
+    for (var boxIdx = 0; boxIdx < block.Boxes.length; boxIdx++) {
+        var box = sudoGrid.AllBoxes[boxIdx];
+        //box.HtmlElement = ??;
+        box.Value = 3;
+        box.UpdateValue();
+    }
+}
+
+var Beginning, Intermediate, Difficult;
+
+
+/*  END GAME LOGIC  */
+
+var cells = document.getElementById('tr')
